8cfa58232e2cd8741509b52c652eaf50665b60e2
Abdalla-Hirzalla <a.hirzalla@hotmail.com>
2021-07-27 17:17:11 +0300

    solve class 03

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-
 import React from 'react';
 import Header from './components/header';
 import Main from './components/main';
 import Footer from './components/footer';
-class App extends React.Component {
+import Data from './components/assets/data.json';
+import SelectedBeast from './components/selectedBeasts';
 
-  render() {
-    return (
-      <div>
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      title: '',
+      imageUrl: '',
+      description: '',
+      modalState: false,
+    };
+  }
+  handler = (title, imageUrl, description, modalState) => {
+      this.setState({
+        title: title,
+        imageUrl: imageUrl,
+        description: description,
+        modalState: modalState,
+      });
+  };
+  handelModalShow = modalState =>{
+      this.setState({
+        modalState: modalState
+      });
+  }
+  render(){
+    return(
+      <>
+        <SelectedBeast 
+          title={this.state.title}
+          imageUrl={this.state.imageUrl}
+          description={this.state.description}
+          showModal={this.state.modalState}
+          hideModal={this.handelModalShow}
+        />
         <Header />
-        <Main />
+        <Main 
+          data={Data}
+          handler={this.handler}/>
         <Footer />
-      </div>
-    )
+      </>
+    );
   }
 }
 export default App;
+
--- src/components/main.js
+++ src/components/main.js
@@ -1,34 +1,34 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Row, Container } from "react-bootstrap";
-import HornedBeasts from './hornedbeasts';
+import HornedBeasts from './hornedBeasts';
 import Data from './assets/data.json';
-
-
 
 
 class Main extends React.Component {
     render() {
         return (
-
-          
-
             <>
                 <Container>
                     <Row>
-                        {Data.map((item, index) => {
-                            return (
-                                <Col lg={4}>
-                                    <HornedBeasts title={item.title} imgUrl={item.image_url} description={item.description} />
-                                </Col>
-                            )
+                        {
+                            this.props.data.map(item => {
+                                return (
+                                    <Col lg={4}>
+                                        <HornedBeasts
+                                            key={item.title.toString()}
+                                            title={item.title}
+                                            imageUrl={item.image_url}
+                                            description={item.description}
+                                            handler={this.props.handler}
+                                        />
+                                    </Col>
+                                )
+                            })
                         }
-
-                        )}
                     </Row>
                 </Container>
             </>
-
         )
     }
 }
